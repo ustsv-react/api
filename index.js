adaa6016a65d8870fc9bb4bd92b7c64170a12b9b
Kelvin Ai <aihao0331@fb.com>
2022-02-22 16:36:27 -0800

    add cookie for cors

--- index.js
+++ index.js
@@ -1,22 +1,33 @@
-const express = require('express');
-const bodyParser = require('body-parser')
+const express = require("express");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 const { getAllOjProblems, getOjProblemById, checkAnswer } = require("./oj");
-const { loginHandler } = require('./login');
+const { loginHandler } = require("./login");
 
 const app = express();
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-app.get('/api/users', (req, res) => {
+app.use((_, res, next) => {
+  if (res.cookies["magic-word"] === "ustsv") {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+
+    next();
+  }
+});
+
+app.get("/api/users", (_, res) => {
   res.status(200).send("hello world from ustsv.");
 });
 
-app.post('/api/login', loginHandler);
+app.post("/api/login", loginHandler);
 
-app.get('/oj/api/problems', getAllOjProblems);
-app.get('/oj/api/problems/:problemId', getOjProblemById);
-app.post('/oj/api/problems/:problemId/answer', checkAnswer);
+app.get("/oj/api/problems", getAllOjProblems);
+app.get("/oj/api/problems/:problemId", getOjProblemById);
+app.post("/oj/api/problems/:problemId/answer", checkAnswer);
 
 app.listen(port, () => {
   console.log(`Express Server is running on port ${port}`);
